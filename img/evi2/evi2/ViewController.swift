3e894db9b45d92b42b8f108ca73d1ccb523ffbac
EricCortez <65201469+EricCortez@users.noreply.github.com>
2021-04-15 00:32:40 -0400

    Primer prototipo
    Podria haber mas cambios

---
+++ img/evi2/evi2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  evi2
+//
+//  Created by user182851 on 4/14/21.
+//  Copyright © 2021 user182851. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
